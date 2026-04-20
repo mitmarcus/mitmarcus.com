a25ce217ed642bd8b2ab93066e466286a4dad8bb
mitmarcus <114725463+mitmarcus@users.noreply.github.com>
2026-04-20 11:19:27 +0200

    feat: add apnea trainer link and separator to footer to differentiate gizmos

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,9 +2,11 @@ import {
 	RiCameraLensFill,
 	RiGithubFill,
 	RiLinkedinBoxFill,
+	RiLungsFill,
 } from 'react-icons/ri';
 
 import Link from '@/components/ui/link';
+import Separator from '@/components/ui/separator';
 import { siteConfig } from '@/config/site';
 
 const Footer = () => {
@@ -20,6 +22,10 @@ const Footer = () => {
 					<Link href={siteConfig.instaUrl} aria-label='photos'>
 						<RiCameraLensFill className='size-6 opacity-50' />
 					</Link>
+					<Link href={siteConfig.apneaUrl} aria-label='apnea trainer'>
+						<RiLungsFill className='size-6 opacity-50' />
+					</Link>
+					<Separator orientation='vertical' className='mx-1 h-5' />
 					<Link href={siteConfig.links.github} aria-label='github'>
 						<RiGithubFill className='size-6' />
 					</Link>
